94e6b01c9b4871f07b674af54ed3b65e90085be4
Chase Terry <cterry@lightbeacon.com>
2024-12-11 15:13:43 -0600

    Installed Laravel. Added Blueprint to create models, migrations, factories, etc.

---
+++ app/Models/Talk.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Talk extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'abstract',
+        'start_time',
+        'end_time',
+        'speaker_id',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'start_time' => 'datetime',
+        'end_time' => 'datetime',
+        'speaker_id' => 'integer',
+    ];
+
+    public function speaker(): BelongsTo
+    {
+        return $this->belongsTo(Speaker::class);
+    }
+
+    public function conferences(): BelongsToMany
+    {
+        return $this->belongsToMany(Conference::class);
+    }
+}
