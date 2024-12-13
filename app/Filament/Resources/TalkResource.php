b0c3d481faf9968d4dffc5d55d1b985071ce6765
Chase Terry <cterry@lightbeacon.com>
2024-12-13 13:35:11 -0600

    Adds table filters for talks.

--- app/Filament/Resources/TalkResource.php
+++ app/Filament/Resources/TalkResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\TalkLength;
 use App\Filament\Resources\TalkResource\Pages;
 use App\Filament\Resources\TalkResource\RelationManagers;
 use App\Models\Talk;
@@ -11,7 +12,7 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class TalkResource extends Resource
 {
@@ -42,29 +43,65 @@ class TalkResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
+            ->persistFiltersInSession()
+            ->filtersTriggerAction(function ($action) {
+                return $action->button()->label('Filters');
+            })
             ->columns([
                 Tables\Columns\TextColumn::make('title')
-                    ->searchable(),
+                    ->searchable()
+                    ->description(function (Talk $record) {
+                        return Str::of($record->abstract)->limit(40);
+                    }),
+                Tables\Columns\ImageColumn::make('speaker.avatar')
+                    ->label('Image')
+                    ->defaultImageUrl(function (Talk $record) {
+                        return 'https://ui-avatars.com/api/?background=0D8ABC&color=fff&name='.urlencode(
+                                $record->speaker->name
+                            );
+                    })
+                    ->circular(),
+                Tables\Columns\TextColumn::make('speaker.name')
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('new_talk')
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('length')
+                    ->icon(function ($state) {
+                        return match ($state) {
+                            TalkLength::NORMAL => 'heroicon-o-megaphone',
+                            TalkLength::LIGHTNING => 'heroicon-o-bolt',
+                            TalkLength::KEYNOTE => 'heroicon-o-key',
+                        };
+                    }),
+                Tables\Columns\TextColumn::make('status')
+                    ->badge()
+                    ->sortable()
+                    ->color(function ($state) {
+                        return $state->getColor();
+                    }),
                 Tables\Columns\TextColumn::make('start_time')
                     ->dateTime()
-                    ->sortable(),
+                    ->sortable()
+                    ->toggleable(),
                 Tables\Columns\TextColumn::make('end_time')
                     ->dateTime()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('speaker.name')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
                     ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    ->toggleable(),
             ])
             ->filters([
-                //
+                Tables\Filters\TernaryFilter::make('new_talk'),
+                Tables\Filters\SelectFilter::make('speaker')
+                    ->relationship('speaker', 'name')
+                    ->multiple()
+                    ->searchable()
+                    ->preload(),
+                Tables\Filters\Filter::make('has_avatar')
+                    ->label('Only speakers with avatars')
+                    ->query(function ($query) {
+                        $query->whereHas('speaker', function (Builder $query) {
+                            $query->whereNotNull('avatar');
+                        });
+                    })
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
